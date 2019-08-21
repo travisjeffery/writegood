0de952f0f9d912438f8195807e45e2462142c7ff
Travis Jeffery <tj@travisjeffery.com>
2019-08-21 05:22:08 -0500

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  context: __dirname + "/app",
+  entry: "./app.js",
+  mode: 'development',
+
+  output: {
+    filename: "app.js",
+    path: __dirname + "/dist",
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react',
+              {
+                "plugins": [
+                  "@babel/plugin-proposal-class-properties"
+                ]
+              },
+            ]
+          }
+        }
+      }
+    ]
+  }
+}
