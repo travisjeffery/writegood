8e31d9c06ace4e52cd3938d111533dee501e35e7
Travis Jeffery <tj@travisjeffery.com>
2019-08-22 19:05:30 -0400

    add a http server

--- dev-resources/user.clj
+++ dev-resources/user.clj
@@ -2,6 +2,9 @@
   (:require
    [clojure.walk :as walk]
    [writegood.schema :as s]
+   [com.walmartlabs.lacinia.pedestal :as lp]
+   [io.pedestal.http :as http]
+   [clojure.java.browse :refer [browse-url]]
    [com.walmartlabs.lacinia :as lacinia])
   (:import
    (clojure.lang IPersistentMap)))
@@ -22,6 +25,32 @@
        :else
        node))
    m))
+
+(defonce server nil)
+
+(defn start-server
+  [_]
+  (let [server (-> schema
+                   (lp/service-map {:graphiql true})
+                   http/create-server
+                   http/start)]
+    (browse-url "http://localhost:8888/")
+    server))
+
+(defn stop-server
+  [server]
+  (http/stop server)
+  nil)
+
+(defn start
+  []
+  (alter-var-root #'server start-server)
+  :started)
+
+(defn stop
+  []
+  (alter-var-root #'server stop-server)
+  :stopped)
 
 (defn q
   [query-string]
